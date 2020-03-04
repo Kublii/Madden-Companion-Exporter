1363cfefb5c49030ff05f44ed450d71d88cb4fd3
Kublii <59628509+Kublii@users.noreply.github.com>
2020-03-04 23:56:47 +0000

    Update app.js
    git add *
    git commit -m "Update app.js with firebase info"
    git Push

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+const serviceAccount = require("./firebase-adminsdk-q747z@groovy-treat-264523.iam.gserviceaccount.com.json");
 
 // TODO: Enter your database url from firebase
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://groovy-treat-264523.firebaseio.com/"
+});
 
 app.set('port', (process.env.PORT || 3001));
 
@@ -168,3 +168,6 @@ app.post('/:username/:platform/:leagueId/team/:teamId/roster', (req, res) => {
 app.listen(app.get('port'), () =>
     console.log('Madden Data is running on port', app.get('port'))
 );
+git add *
+git commit -m "Update app.js with firebase info"
+git Push
